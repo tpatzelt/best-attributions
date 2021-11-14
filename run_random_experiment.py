c08d27ef6c301dba6b85b3325cec04f39be149a5
Tim Patzelt <patzelt.tim@gmail.com>
2021-11-14 22:14:34 +0100

    wip

--- run_random_experiment.py
+++ run_random_experiment.py
@@ -88,4 +88,53 @@ with open("../data/random_one_sample.json", "w") as fp:
                    lig_tpn=lig_tpn, lig_tps=lig_tps),
               fp)
 
+
+### PART 2
 # In[ ]:
+model = load_distilbert(return_softmax=1)
+data = load_imdb_albert_lig_data()
+
+observations, lig_attributions = zip(*[extract_token_ids_and_attributions(d) for d in data[:2000]])
+del data
+
+evaluator = ProportionalityEvaluator(model=model, baseline_factory=ZeroBaselineFactory)
+
+tpn_results, tps_results = [], []
+for i in tqdm(range(len(observations))):
+    observation = observations[i]
+    lig_attribution = lig_attributions[i]
+    random_attribution = RandomAttributionValues.get_attribution_values(observation=observation)
+    tpn_results.append((
+        evaluator.compute_tpn(observation=observation, attribution_values=lig_attribution),
+        evaluator.compute_tpn(observation=observation, attribution_values=random_attribution)))
+    tps_results.append((
+        evaluator.compute_tps(observation=observation, attribution_values=lig_attribution),
+        evaluator.compute_tps(observation=observation, attribution_values=random_attribution)))
+
+# In[ ]:
+
+with open("../data/random_all_samples-softmax.json", "w") as fp:
+    json.dump(dict(tpn_results=tpn_results, tps_results=tps_results), fp)
+
+# In[ ]:
+
+
+iterations = 1000
+random_index = 42
+observation= observations[random_index]
+lig_tpn, lig_tps = tpn_results[random_index], tps_results[42]
+random_attributions = [RandomAttributionValues.get_attribution_values(observation) for _ in
+                       range(iterations)]
+random_tpn_results, random_tps_results = [], []
+for attribution in tqdm(random_attributions):
+    random_tpn_results.append(
+        evaluator.compute_tpn(observation=observation, attribution_values=attribution))
+    random_tps_results.append(
+        evaluator.compute_tps(observation=observation, attribution_values=attribution))
+
+# In[ ]:
+
+with open("../data/random_one_sample-softmax.json", "w") as fp:
+    json.dump(dict(random_tpn_results=random_tpn_results, random_tps_results=random_tps_results,
+                   lig_tpn=lig_tpn, lig_tps=lig_tps),
+              fp)
