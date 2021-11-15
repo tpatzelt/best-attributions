9559d21ea6546cc3efef0948f75c434235b35929
Tim Patzelt <patzelt.tim@gmail.com>
2021-11-15 07:51:29 +0100

    fix path

--- run_random_experiment.py
+++ run_random_experiment.py
@@ -40,7 +40,7 @@ model = load_distilbert()
 # In[12]:
 
 
-observations, lig_attributions = zip(*[extract_token_ids_and_attributions(d) for d in data[:2000]])
+observations, lig_attributions = zip(*[extract_token_ids_and_attributions(d) for d in data[:1000]])
 del data
 
 evaluator = ProportionalityEvaluator(model=model, baseline_factory=ZeroBaselineFactory)
@@ -62,11 +62,15 @@ for i in tqdm(range(len(observations))):
 
 # In[ ]:
 
-with open("../data/random_all_samples.json", "w") as fp:
+with open("data/random_all_samples.json", "w") as fp:
     json.dump(dict(tpn_results=tpn_results, tps_results=tps_results), fp)
 
-# In[ ]:
-
+for lig, random in tpn_results:
+    ex.log_scalar("lig-tpn-all", lig)
+    ex.log_scalar('random-tpn-all', random)
+for lig, random in tps_results:
+    ex.log_scalar("lig-tps-all", lig)
+    ex.log_scalar('random-tps-all', random)
 
 iterations = 1000
 random_index = 42
@@ -83,18 +87,21 @@ for attribution in tqdm(random_attributions):
 
 # In[ ]:
 
-with open("../data/random_one_sample.json", "w") as fp:
+with open("data/random_one_sample.json", "w") as fp:
     json.dump(dict(random_tpn_results=random_tpn_results, random_tps_results=random_tps_results,
                    lig_tpn=lig_tpn, lig_tps=lig_tps),
               fp)
-
+_log_scores(name="random-tpn-one", scores=random_tpn_results)
+_log_scores(name="random-tps-one", scores=random_tps_results)
+_log_scores(name="lig-tpn-one", scores=lig_tpn*len(random_tpn_results))
+_log_scores(name="lig-tps-one", scores=lig_tps*len(random_tpn_results))
 
 ### PART 2
 # In[ ]:
 model = load_distilbert(return_softmax=1)
 data = load_imdb_albert_lig_data()
 
-observations, lig_attributions = zip(*[extract_token_ids_and_attributions(d) for d in data[:2000]])
+observations, lig_attributions = zip(*[extract_token_ids_and_attributions(d) for d in data[:1000]])
 del data
 
 evaluator = ProportionalityEvaluator(model=model, baseline_factory=ZeroBaselineFactory)
@@ -113,11 +120,16 @@ for i in tqdm(range(len(observations))):
 
 # In[ ]:
 
-with open("../data/random_all_samples-softmax.json", "w") as fp:
+with open("data/random_all_samples-softmax.json", "w") as fp:
     json.dump(dict(tpn_results=tpn_results, tps_results=tps_results), fp)
 
 # In[ ]:
-
+for lig, random in tpn_results:
+    ex.log_scalar("lig-tpn-all-softmax", lig)
+    ex.log_scalar('random-tpn-all-softmax', random)
+for lig, random in tps_results:
+    ex.log_scalar("lig-tps-all-softmax", lig)
+    ex.log_scalar('random-tps-all-softmax', random)
 
 iterations = 1000
 random_index = 42
@@ -134,7 +146,11 @@ for attribution in tqdm(random_attributions):
 
 # In[ ]:
 
-with open("../data/random_one_sample-softmax.json", "w") as fp:
+with open("data/random_one_sample-softmax.json", "w") as fp:
     json.dump(dict(random_tpn_results=random_tpn_results, random_tps_results=random_tps_results,
                    lig_tpn=lig_tpn, lig_tps=lig_tps),
               fp)
+_log_scores(name="random-tpn-one-softmax", scores=random_tpn_results)
+_log_scores(name="random-tps-one-softmax", scores=random_tps_results)
+_log_scores(name="lig-tpn-one-softmax", scores=lig_tpn*len(random_tpn_results))
+_log_scores(name="lig-tps-one-softmax", scores=lig_tps*len(random_tpn_results))
