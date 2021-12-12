51dec50b802b4f208ea1dd315d4a7a73a0a246ab
tpatzelt <tpatzelt@uos.de>
2021-12-12 19:22:37 +0100

    Add REPO_PATH to cfg.

--- cfg.py
+++ cfg.py
@@ -1,5 +1,10 @@
-user="dbUser"
-password="dbUserPassword"
-DB_URI=f"mongodb+srv://{user}:{password}@best-attributions.oglb5.mongodb.net/best" \
-        "-attributions?retryWrites=true&w=majority"
-DB_NAME="best-attributions"
+import os
+
+user = "dbUser"
+password = "dbUserPassword"
+DB_URI = f"mongodb+srv://{user}:{password}@best-attributions.oglb5.mongodb.net/best" \
+         "-attributions?retryWrites=true&w=majority"
+DB_NAME = "best-attributions"
+
+REPO_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(REPO_PATH, "data")
