ed0d36f13dd94bbad7af3618d6103739463fe454
tpatzelt <tpatzelt@uos.de>
2021-12-12 19:27:24 +0100

    Add shell script to run all experiments.

---
+++ run_all_experiments.sh
@@ -0,0 +1,6 @@
+source venv/bin/activate
+python3.9 run_experiment.py with random_config
+python3.9 run_experiment.py with kernel_shap_config
+python3.9 run_experiment.py with lime_config
+python3.9 run_experiment.py with custom_hill_climber_tpn_config
+python3.9 run_experiment.py with custom_hill_climber_tps_config
