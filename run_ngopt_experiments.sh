490af70b97453a6134e42743b990eedcd52f39e7
Tim Patzelt <patzelt.tim@gmail.com>
2021-12-16 20:19:51 +0100

    First visualization attempts.

---
+++ run_ngopt_experiments.sh
@@ -0,0 +1,2 @@
+python3.9 run_experiment.py with ngopt_tpn_config only_first_sentence=True dataset.num_samples=100 attribution_method.budget=500 --name=ngopt_tpn_first_sentence_config
+python3.9 run_experiment.py with ngopt_tpn_config --name=ngopt_tpn_config
